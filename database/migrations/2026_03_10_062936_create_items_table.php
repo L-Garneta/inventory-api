bffc782cbce18cfbc7420cf7ab34c2d8eedd0a59
L-Garneta <garnetamahes05@gmail.com>
2026-03-11 13:43:31 +0700

    tabel trans masuk

--- database/migrations/2026_03_10_062936_create_items_table.php
+++ database/migrations/2026_03_10_062936_create_items_table.php
@@ -6,20 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->string('kode')->unique();
+            $table->string('nama');
+            $table->string('kategori')->nullable();
+            $table->string('ruangan')->nullable();
+            $table->string('satuan')->nullable();
+            $table->decimal('harga_beli', 15, 2)->default(0);
+            $table->decimal('harga_jual', 15, 2)->default(0);
+            $table->integer('stok')->default(0);
+            $table->integer('stok_minimum')->default(0);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('items');
---
+++ database/migrations/2026_03_11_060520_create_transaksi_masuk_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+{
+    Schema::create('transaksi_masuk', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('item_id')->constrained('items')->cascadeOnDelete();
+        $table->date('tanggal');
+        $table->integer('jumlah');
+        $table->string('supplier')->nullable();
+        $table->text('keterangan')->nullable();
+        $table->timestamps();
+    });
+}
+
+public function down(): void
+{
+    Schema::dropIfExists('transaksi_masuk');
+}
+};
